b88fc0183b48bde7d84368553ddf6fc73a279fce
hdreihah <holger.dreihahn@esa.int>
2025-05-22 14:50:24 +0200

    Moved cdds.tmpl to src/main/resources

--- generate-doc.sh
+++ generate-doc.sh
@@ -5,6 +5,6 @@ echo "Generate proto documtation..."
 protoc  \
  --plugin=protoc-gen-doc=$(which protoc-gen-doc) \
  --doc_out=./target/generated-docs \
- --doc_opt=cdds.tmpl,index.html \
+ --doc_opt=src/main/resources/cdds.tmpl,cdds.html \
  -I=src/main/protobuf \
  src/main/protobuf/telemetry.proto

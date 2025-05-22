42c8f05e86f247d4cd0aea55bd9c45f89c270f23
hdreihah <holger.dreihahn@esa.int>
2025-05-22 14:43:50 +0200

    Documentation generation and proto specrefinment

--- generate-doc.sh
+++ generate-doc.sh
@@ -1,5 +1,10 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 echo "Generate proto documtation..."
 
-protoc   --plugin=protoc-gen-doc=/home/holger/tools/protoc-gen-doc/protoc-gen-doc-1.5.1   --doc_out=target/doc   --doc_opt=html,index.html -I=src/main/protobuf  src/main/protobuf/telemetry.proto
+protoc  \
+ --plugin=protoc-gen-doc=$(which protoc-gen-doc) \
+ --doc_out=./target/generated-docs \
+ --doc_opt=cdds.tmpl,index.html \
+ -I=src/main/protobuf \
+ src/main/protobuf/telemetry.proto

dfeb9099832226d9f6e35dae6d732515c3b148f6
hdreihah <holger.dreihahn@esa.int>
2026-05-27 23:40:08 +0200

    Allow building without dcoumentation generation

--- generate-doc.sh
+++ generate-doc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn generate-sources $@ -Dprotoc-gen-doc=/Users/Shared/tools/protoc-gen-doc/1.5.2/protoc-gen-doc -Dsort-source=:sort=source
+mvn verify $@ -Dprotoc-gen-doc=/Users/Shared/tools/protoc-gen-doc/1.5.2/protoc-gen-doc -Dsort-source=:sort=source -DskipTests

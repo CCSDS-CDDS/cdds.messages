b88f1098aff11ec2a3b6ab87c8ce16255b551451
hdreihah <holger.dreihahn@esa.int>
2026-05-27 23:57:36 +0200

    Externalize path to protoc-gen-doc 1.5.2 into generate-doc.sh

--- generate-doc.sh
+++ generate-doc.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-mvn verify $@ -Dprotoc-gen-doc=/Users/Shared/tools/protoc-gen-doc/1.5.2/protoc-gen-doc -Dsort-source=:sort=source -DskipTests
+mvn verify $@ -Dprotoc-gen-doc=/Users/Shared/tools/protoc-gen-doc/1.5.2/protoc-gen-doc \
+    -Dprotoc-gen-doc=/Users/Shared/tools/protoc-gen-doc/1.5.2/protoc-gen-doc \
+    -Dsort-source=:sort=source -DskipTests
